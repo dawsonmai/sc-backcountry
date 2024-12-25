14cc07125c66c3d1fb3143557ea1ac3ed4c7a54f
Tina Mai <tinabmai@gmail.com>
2024-12-24 16:14:18 -0800

    fixing up cover layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,17 @@ import AreaData from "@/components/home/AreaData";
 import TransitionImage from "@/components/home/TransitionText";
 import ExploreArea from "@/components/home/ExploreArea";
 import ConditionsPreview from "@/components/home/ConditionsPreview";
-export default function Home () {
-  return (
-    <div className="flex flex-col h-full p-5">
-      <Header />
-      <IntroImage />
-      <AreaData />
-      <TransitionImage />
-      <ExploreArea />
-      <ConditionsPreview />
-    </div>
-  );
+export default function Home() {
+	return (
+		<div className="flex flex-col px-5 pb-5">
+			<div className="flex flex-col min-h-[calc(100dvh)] py-5">
+				<Header />
+				<IntroImage />
+			</div>
+			<AreaData />
+			<TransitionImage />
+			<ExploreArea />
+			<ConditionsPreview />
+		</div>
+	);
 }
--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,17 +1,17 @@
 const Header = () => {
-    return(
-        <div className="flex sm:flex-row flex-col items-center sm:justify-between pb-10 bg-transparent">
-            <h1 className="text-lg font-semibold">SC Backcountry</h1>
+	return (
+		<div className="flex sm:flex-row flex-col items-center sm:justify-between pb-5 bg-transparent">
+			<h1 className="text-lg font-semibold">SC Backcountry</h1>
 
-            <div className=" flex text-sm gap-5 items-center">
-                <p>Routes</p>
-                <p>Conditions</p>
-                <p>Education</p>
-                <p>About</p>
-                <button className="bg-stone-800 text-white px-4 py-1 rounded-full">Contact</button>
-            </div>
-        </div>
-    )
-}
+			<div className=" flex text-sm gap-5 items-center">
+				<p>Routes</p>
+				<p>Conditions</p>
+				<p>Education</p>
+				<p>About</p>
+				<button className="bg-stone-800 text-white px-4 py-1 rounded-full">Contact</button>
+			</div>
+		</div>
+	);
+};
 
-export default Header
+export default Header;
