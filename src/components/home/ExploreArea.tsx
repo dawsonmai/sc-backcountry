e2dfd51fc904e699fd874130d0b078a1e7053b70
Dawson Mai <dawsonmmai@gmail.com>
2025-03-12 21:38:00 -0700

    home page area boxes more adaptable for mediums size screens

--- src/components/home/ExploreArea.tsx
+++ src/components/home/ExploreArea.tsx
@@ -6,16 +6,16 @@ const ExploreArea = () => {
 			<div className="h-full md:w-1/5 lg:w-1/5">
 				<h2 className="uppercase font-semibold text-lg sm:text-xl tracking-wider">Explore By Region</h2>
 			</div>
-			<div className="w-full md:w-3/4 items-center flex justify-center ">
-				<div className="flex flex-col md:flex-row md:w-full lg:w-full p-4 gap-5 lg:gap-8">
-					<div className="h-96 border-2 border-gray-400 rounded-3xl flex flex-col w-1/3">
+			<div className="w-full md:w-3/4 items-center flex justify-center">
+				<div className="flex flex-row md:w-full lg:w-full p-4 gap-5 lg:gap-8">
+					<div className="h-96 md:h-80 lg:h-96 border-2 border-gray-400 rounded-3xl flex flex-col w-1/3">
 						<div className="flex-1 bg-gray-400 rounded-2xl" style={{
 							backgroundImage: "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(SanBernardinoMountainsAerial.jpeg)",
 							backgroundSize: "cover",
 							backgroundPosition: "bottom",
 						}} />
 						<div className="flex w-full p-3 justify-between items-center">
-							<h3 className="text-lg md:text-lg lg:text-2xl pr-4">San Bernardino Mountains</h3>
+							<h3 className="text-lg md:text-lg lg:text-2xl">San Bernardino Mountains</h3>
 							<a href="/routes/san-bernardino-mountains">
 								<button className="flex items-center justify-center">
 									<ArrowRight className="size-9 text-white bg-black rounded-full p-2" />
@@ -24,7 +24,7 @@ const ExploreArea = () => {
 						</div>
 					</div>
 
-					<div className="h-96 border-2 border-gray-400 rounded-3xl flex flex-col w-1/3">
+					<div className="h-96 md:h-80 lg:h-96 border-2 border-gray-400 rounded-3xl flex flex-col w-1/3">
 						<div className="flex-1 bg-gray-400 rounded-2xl" style={{
 							backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0)), url(SanJacintoMountainsAerial.jpeg)",
 							backgroundSize: "cover",
@@ -40,7 +40,7 @@ const ExploreArea = () => {
 						</div>
 					</div>
 
-					<div className="h-96 border-2 border-gray-400 rounded-3xl flex flex-col w-1/3">
+					<div className="h-96 md:h-80 lg:h-96 border-2 border-gray-400 rounded-3xl flex flex-col w-1/3">
 						<div className="flex-1 bg-gray-400 rounded-2xl" style={{
 							backgroundImage: "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(SanGabrielMountainsAerial.jpeg)",
 							backgroundSize: "cover",
