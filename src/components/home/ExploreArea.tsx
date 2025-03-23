57e03ae3e80b04a2ffdbe0f5e68d49b2ea3d09d6
Dawson Mai <dawsonmmai@gmail.com>
2025-03-23 10:59:12 -0700

    cleaner explore area grid design

--- src/components/home/ExploreArea.tsx
+++ src/components/home/ExploreArea.tsx
@@ -1,4 +1,6 @@
 import { ArrowRight } from "lucide-react";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 const ExploreArea = () => {
 	return (
@@ -7,52 +9,51 @@ const ExploreArea = () => {
 				<h2 className="uppercase font-semibold text-lg sm:text-xl tracking-wider">Explore By Region</h2>
 			</div>
 			<div className="w-full md:w-3/4 items-center flex justify-center">
-				<div className="grid grid-cols-2 sm:grid-cols-3 p-4 gap-5 lg:gap-8 w-full">
-					<div className="h-72 md:h-80 lg:h-96 border-2 border-gray-400 rounded-3xl flex flex-col w-full">
-						<div className="flex-1 bg-gray-400 rounded-2xl" style={{
-							backgroundImage: "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(SanBernardinoMountainsAerial.jpeg)",
-							backgroundSize: "cover",
-							backgroundPosition: "bottom",
-						}} />
-						<div className="flex w-full p-3 justify-between items-center">
-							<h3 className="text-md md:text-lg lg:text-2xl">San Bernardino Mountains</h3>
-							<a href="/routes/san-bernardino-mountains">
-								<button className="flex items-center justify-center">
-									<ArrowRight className="size-9 text-white bg-black rounded-full p-2" />
-								</button>
-							</a>
+				<div className="grid grid-cols-2 sm:grid-cols-3 md:p-4 gap-5 lg:gap-8 w-full">
+					<div className="h-72 md:h-80 lg:h-96 rounded-3xl flex flex-col justify-between w-full shadow-lg shadow-neutral-300 bg-neutral-50 p-3">
+						<div className="h-3/4">
+							<div className="bg-gray-400 rounded-2xl h-3/4" style={{
+								backgroundImage: "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(SanBernardinoMountainsAerial.jpeg)",
+								backgroundSize: "cover",
+								backgroundPosition: "bottom",
+							}} />
+							<h3 className="sm:text-md md:text-lg lg:text-xl p-2 w-full">San Bernardino Mountains</h3>
+						</div>
+						<div className="flex justify-end">
+							<Button className="bg-transparent border-2 border-black rounded-full py-2 px-4 font-medium text-black hover:bg-black hover:text-white font-normal text-md">
+								<Link href="/routes/san-bernardino-mountains"><ArrowRight strokeWidth={2.5} className="size-9 p-0" /></Link>
+							</Button>
+						</div>
+					</div>
+					<div className="h-72 md:h-80 lg:h-96 rounded-3xl flex flex-col justify-between w-full shadow-lg shadow-neutral-300 bg-neutral-50 p-3">
+						<div className="h-3/4">
+							<div className="bg-gray-400 rounded-2xl h-3/4" style={{
+								backgroundImage: "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(SanGabrielMountainsAerial.jpeg)",
+								backgroundSize: "cover",
+								backgroundPosition: "bottom",
+							}} />
+							<h3 className="sm:text-md md:text-lg lg:text-xl p-2 w-full">San Gabriel Mountains</h3>
+						</div>
+						<div className="flex justify-end">
+							<Button className="bg-transparent border-2 border-black rounded-full py-2 px-4 font-medium text-black hover:bg-black hover:text-white font-normal text-md">
+								<Link href="/routes/san-gabriel-mountains"><ArrowRight strokeWidth={2.5} className="size-9 p-0" /></Link>
+							</Button>
 						</div>
 					</div>
 
-					<div className="h-72 md:h-80 lg:h-96 border-2 border-gray-400 rounded-3xl flex flex-col w-full">
-						<div className="flex-1 bg-gray-400 rounded-2xl" style={{
-							backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0)), url(SanJacintoMountainsAerial.jpeg)",
-							backgroundSize: "cover",
-							backgroundPosition: "center",
-						}} />
-						<div className="flex w-full p-3 justify-between items-center">
-							<h3 className="text-md md:text-lg lg:text-2xl pr-4">San Jacinto Mountains</h3>
-							<a href="/routes/san-jacinto-mountains">
-								<button className="flex items-center justify-center">
-									<ArrowRight className="size-9 text-white bg-black rounded-full p-2" />
-								</button>
-							</a>
+					<div className="h-72 md:h-80 lg:h-96 rounded-3xl flex flex-col justify-between w-full shadow-lg shadow-neutral-300 bg-neutral-50 p-3">
+						<div className="h-3/4">
+							<div className="bg-gray-400 rounded-2xl h-3/4" style={{
+								backgroundImage: "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(SanJacintoMountainsAerial.jpeg)",
+								backgroundSize: "cover",
+								backgroundPosition: "bottom",
+							}} />
+							<h3 className="sm:text-md md:text-lg lg:text-xl p-2 w-full">San Jacinto Mountains</h3>
 						</div>
-					</div>
-
-					<div className="h-72 md:h-80 lg:h-96 border-2 border-gray-400 rounded-3xl flex flex-col w-full">
-						<div className="flex-1 bg-gray-400 rounded-2xl" style={{
-							backgroundImage: "linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(SanGabrielMountainsAerial.jpeg)",
-							backgroundSize: "cover",
-							backgroundPosition: "center",
-						}} />
-						<div className="flex w-full p-3 justify-between items-center">
-							<h3 className="text-md md:text-lg lg:text-2xl pr-4">San Gabriel Mountains</h3>
-							<a href="/routes/san-gabriel-mountains">
-								<button className="flex items-center justify-center">
-									<ArrowRight className="size-9 text-white bg-black rounded-full p-2" />
-								</button>
-							</a>
+						<div className="flex justify-end">
+							<Button className="bg-transparent border-2 border-black rounded-full py-2 px-4 font-medium text-black hover:bg-black hover:text-white font-normal text-md">
+								<Link href="/routes/san-jacinto-mountains"><ArrowRight strokeWidth={2.5} className="size-9 p-0" /></Link>
+							</Button>
 						</div>
 					</div>
 				</div>
