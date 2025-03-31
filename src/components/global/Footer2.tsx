30d33435eceaaf2691ea37b4a83018bd26b6bc27
Dawson Mai <dawsonmmai@gmail.com>
2025-03-31 12:58:24 -0700

    instagram link added

--- src/components/global/Footer2.tsx
+++ src/components/global/Footer2.tsx
@@ -32,9 +32,11 @@ const Footer2 = () => {
                         <Button className="mb-6 bg-transparent border-2 border-black rounded-full py-2 px-4 font-medium text-black hover:bg-black hover:text-white font-normal text-md">
                             <Link href="mailto:scbackcountry@gmail.com">Contact Us</Link>
                         </Button>
-                        <div className="bg-gray-200 rounded-full w-9 h-9 flex flex-row justify-center items-center">
-                            <Instagram strokeWidth={1.5} className="size-5" />
-                        </div>
+                        <a href="https://apac01.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.instagram.com%2Fscbackcountry%3Figsh%3DNTc4MTIwNjQ2YQ%253D%253D%26utm_source%3Dqr&data=05%7C02%7C%7Ce2b905c965fe425c686708dd708e377f%7C84df9e7fe9f640afb435aaaaaaaaaaaa%7C1%7C0%7C638790478301747394%7CUnknown%7CTWFpbGZsb3d8eyJFbXB0eU1hcGkiOnRydWUsIlYiOiIwLjAuMDAwMCIsIlAiOiJXaW4zMiIsIkFOIjoiTWFpbCIsIldUIjoyfQ%3D%3D%7C0%7C%7C%7C&sdata=xkdHFlaNfwzTHoyWW0dcfuaexJ2DySBXvgLW%2FmHH4%2BA%3D&reserved=0">
+                            <div className="bg-gray-200 rounded-full w-9 h-9 flex flex-row justify-center items-center">
+                                <Instagram strokeWidth={1.5} className="size-5" />
+                            </div>
+                        </a>
                     </div>
                     <p className="p-3 font-light text-gray-600 text-sm text-end">&copy;2025 SC Backcountry</p>
                 </div>
